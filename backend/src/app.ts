7554825f5aac7694e31480324c7a9ad754a25f5e
Vanildo <vanildoantonio19@gmail.com>
2026-06-17 21:47:39 +0100

    fix backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { config } from "./config";
+import { config } from "./config/index.js";
 import { errorHandler } from "./middleware/error.middleware";
 
 import authRoutes from "./modules/auth/auth.routes";
